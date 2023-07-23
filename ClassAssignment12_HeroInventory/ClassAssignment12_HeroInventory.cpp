759f6b10a626d95522987bf0eec843e77e0331c1
TrevSh <trevonshelby@outlook.com>
2023-07-23 17:45:34 -0500

    Added notes to explain arrays

--- ClassAssignment12_HeroInventory/ClassAssignment12_HeroInventory.cpp
+++ ClassAssignment12_HeroInventory/ClassAssignment12_HeroInventory.cpp
@@ -4,9 +4,9 @@
 int main()
 {
     const int MAX_ITEMS = 10;
-    std::string inventory[MAX_ITEMS];           //Creates an array with MAX_ITEMS defining how many values it can hold (being 10).
-
-    int numItems = 0;
+    std::string inventory[MAX_ITEMS];           //Creates an array with MAX_ITEMS defining how many values it can hold (being 10). 
+    //Can also initialize items in array like this:    string inventory[MAX_ITEMS] = { "sword", "armor", "shield" };
+    int numItems = 0;                           //numItems is the number of items the Hero is CURRENTLY holding. Not to be confused with the MAX_ITEMS.
     inventory[numItems++] = "sword";
     inventory[numItems++] = "armor";
     inventory[numItems++] = "shield";
@@ -18,7 +18,7 @@ int main()
         std::cout << inventory[i] << "\n";
     }
     std::cout << "\nYou traded your sword for a battle axe.";
-    inventory[0] = "battle axe";
+    inventory[0] = "battle axe";                //Arrays start at [0]. So the first item is really Item 0; not 1.
     std::cout << "\nYour Items: \n";
     for (int i = 0; i < numItems; i++) {
         std::cout << inventory[i] << "\n";
